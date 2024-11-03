e420f151634e6547e29a762af89833727601087b
0jas0jas <ojas1807@gmail.com>
2024-11-03 18:14:13 -0500

    added ojas logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -53,7 +53,6 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
             <p className="font-bold text-inherit">ojas</p>
           </NextLink>
         </NavbarBrand>
