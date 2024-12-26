e27a8c36e26ad1743fd2484cfe8f15843b037b09
Ojas Srivastava <ojas1807@gmail.com>
2024-12-25 20:56:49 -0500

    Rebuild everything

---
+++ app/algebra/page.tsx
@@ -0,0 +1,19 @@
+import { title } from "@/components/primitives";
+import { Divider } from "@nextui-org/divider";
+
+export default function AlgebraPage() {
+  return (
+    <div>
+      <h1 className={title()}>An article about isomorphism and cool things</h1>
+      <Divider className="my-4"/>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec auctor
+        augue eget nunc vehicula, sit amet fermentum libero dictum. Nulla
+        facilisi. Nullam non ligula ac elit ultricies fermentum. Sed sit amet
+        turpis eget nunc vehicula, sit amet fermentum libero dictum. Nulla
+        facilisi. Nullam non ligula ac elit ultricies fermentum. Sed sit amet
+        turpis
+      </p>
+      </div>
+  );
+}
