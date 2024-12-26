8002965cffb5dd26e76e5614fb8ef2aa885b45fb
Ojas Srivastava <ojas1807@gmail.com>
2024-12-25 22:01:36 -0500

    If this doesn't work, I'm leaving

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from '@nextui-org/theme';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -10,11 +10,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
+        sans: ['var(--font-sans)'],
+        mono: ['var(--font-mono)'],
       },
     },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [nextui()],
-}
+};
+
+export default config;
+
