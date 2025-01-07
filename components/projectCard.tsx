bbd21a373138455d1c7f704d7b5bae88814f462e
Ojas Srivastava <ojas1807@gmail.com>
2025-01-06 19:37:21 -0500

    Theme upgrade

--- components/projectCard.tsx
+++ components/projectCard.tsx
@@ -17,13 +17,13 @@ export const ProjectCard: FC<projectCardProps> = ({
 }) => {
     console.log("Hereeee");
     return (
-        <Card className="w-[1000px] p-6">
+        <Card className=" p-4 grid grid-cols-2 gap-6">
             <CardHeader className="justify-between">
                 <div className="flex gap-5">
                     <div className="flex flex-col gap-2 items-start justify-center">
-                        <h4 className="text-5xl font-semibold leading-none text-default-600">{projectName}</h4>
+                        <h4 className="text-6xl font-semibold leading-none text-default-600">{projectName}</h4>
                         <h4 className="text-large font-semibold text-right leading-none text-default-300">{monthYear}</h4>
-                        <h5 className="text-2xl tracking-tight text-default-500">{skills}</h5>
+                        <h5 className="text-3xl tracking-tight text-default-500">{skills}</h5>
                     </div>
                 </div>
                 {/* <Button
@@ -37,13 +37,14 @@ export const ProjectCard: FC<projectCardProps> = ({
                     {isFollowed ? "Unfollow" : "Follow"}
                 </Button> */}
             </CardHeader>
-            <CardBody className="px-3 py-0 text-small text-default-400">
+            <CardBody className="p-3 py-0 text-default-400">
+                <div className="h-12"></div>
                 <p>{desc}</p>
             </CardBody>
-            <CardFooter className="gap-3">
+            <CardFooter className="px-3 gap-3">
                 <Link href={url}>
                     <Button className="flex gap-1" color="primary">
-                        <p className="">GitHub</p>
+                        <p className="text-default ">GitHub</p>
                     </Button>
                 </Link>
                 {/* <div className="flex gap-1">
